b50f3e7bf1b12932bd4767ae5f7d8f9a6f2ce771
Karsten Marck <karstenmarck611@gmail.com>
2023-06-09 10:51:09 +0200

    test

--- index.php
+++ index.php
@@ -33,8 +33,14 @@
     </form>
 <script>
         const selectElement = document.getElementById('regio');
+        const storedValue = localStorage.getItem('selectedRegio');
+        if (storedValue) {
+            selectElement.value = storedValue;
+        }
+
         selectElement.addEventListener('change', () => {
             const selectedValue = selectElement.value;
+            localStorage.setItem('selectedRegio', selectedValue);
             const baseUrl = window.location.href.split('?')[0]; // Remove existing query parameters if any
             const newUrl = `${baseUrl}?regio=${encodeURIComponent(selectedValue)}`;
             window.location.href = newUrl;
