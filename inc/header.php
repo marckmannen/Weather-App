207166e9d7d44f40ab5b155ef2cf9b50e0cdb851
Karsten Marck <karstenmarck611@gmail.com>
2023-06-09 23:45:21 +0200

    Cleaned up

--- inc/header.php
+++ inc/header.php
@@ -9,6 +9,5 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <script src="./js/script.js"></script>
     <title>Weather App</title>
 </head>
